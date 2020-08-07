cc7c7af85c6b40bee155540be1252f78e548a630
Spliterash <me@spliterash.ru>
2020-08-07 18:55:23 +0400

    Забыл добавить переопределение equals у sender'а

--- SpongeLauncher/src/main/java/ru/spliterash/vkchat/launchers/sponge/SpongeSender.java
+++ SpongeLauncher/src/main/java/ru/spliterash/vkchat/launchers/sponge/SpongeSender.java
@@ -6,9 +6,24 @@ import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.serializer.TextSerializers;
 import ru.spliterash.vkchat.wrappers.AbstractSender;
 
+import java.util.Objects;
+
 public class SpongeSender implements AbstractSender {
     @Getter
     private final CommandSource sender;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SpongeSender that = (SpongeSender) o;
+        return Objects.equals(sender, that.sender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sender);
+    }
 
     public SpongeSender(CommandSource source) {
         this.sender = source;
